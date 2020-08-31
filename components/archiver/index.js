c8b420256352ea623a3c206a3e30f4a95a94cedf
E <endnch@gmail.com>
2020-08-31 12:22:00 -0400

    Limpiar Archiver

--- components/archiver/index.js
+++ components/archiver/index.js
@@ -1,21 +1,16 @@
 /**
  * Archivador de Hispachan Files
- * 
+ *
  * Aquí es donde ocurre toda la magia
  */
 'use strict';
 
-const mongoose = require('mongoose');
 const mkdirp   = require('mkdirp');
 const fs       = require('fs');
-const async    = require('async');
 const request  = require('request');
-const glob     = require('glob');
-const crypto   = require('crypto');
+const md5      = require('md5');
 
 const Thread   = require('../../models/thread');
-const ArchiverTimer = require('./timer');
-const Elapsed  = require('elapsed');
 
 class Archiver {
     constructor() {
@@ -38,91 +33,37 @@ class Archiver {
     }
 
     // Archivar Hilo
-    start() {
-        let that = this;
-        let thread = this.current;
+    async start() {
+        const thread = this.current;
         // Establecer directorios para los datos del hilo
         thread.dataDir = `data/${thread.data.board}/${thread.data.postId}/`;
         thread.fileDir = thread.dataDir + 'src/';
         thread.thumbDir = thread.dataDir + 'thumb/';
-        this.current.saved = 0;
-        this.current.files = [];
-        this.current.thumbs = [];
-        
+        thread.saved = 0;
+
         // Almacenar todas las imágenes
-        async.each(thread.data.replies, (el, cb) => {
-            if (!el.file) {
-                async.setImmediate(() => {
-                    that.reportProgress('Guardando archivos...', ++that.current.saved, that.current.data.replyCount + 1);
-                    cb(null, el);
-                });
+        for (const reply of thread.data.replies) {
+            if (reply.file) {
+                await this.storeAttachment(reply);
             }
-            else {
-                that.storeAttachment(el, cb);
-            }
-        }, ()=> async.waterfall([
-            // Almacenar imagen de OP
-            cb=> that.storeAttachment(thread.data, cb),
-            // Almacenar en la base de datos
-            (data, cb)=> {
-                // Primero revisar si ya estaba
-                let query = {'postId': data.postId, 'board': data.board};
-                Thread.findOne(query, (err, d)=>
-                {
-                    if(d)
-                    {
-                        // El hilo ya estaba almacenado, actualizar
-                        data.lastUpdate = Date.now();
-                        Thread.update(query, data, cb)
-                    }
-                    else
-                    {
-                        // El hilo se almacena por primera vez
-                        new Thread(data).save(cb);
-                    }
-                });
-            },
-            // Eliminar archivos que estén de mas (parece que no funciona elmio)
-            // (data, cb)=> glob(that.current.dataDir + '*/*', cb),
-            /*(files, cb)=> {
-                // Fusión!
-                let allFiles = [...that.current.files, ...that.current.thumbs];
-                async.each(allFiles, (file, callback)=> {
-                    let fname = file.split('/').reverse()[0];
-                    if(allFiles.indexOf(fname) < 0)
-                    {
-                        // Borra esa mierda
-                        fs.unlink(file, callback);
-                    }
-                    else
-                    {
-                        callback();
-                    }
-                }, cb);
-            }*/
-        ], err =>
-        {
-            if(err)
-            {
-                // Hubo un error
-                if(that.current.by.connected) that.current.by.emit('queueFailed', 'Se ha producido un error en el servidor.');
-                return;
-            }
-            // Verificar si el hilo tiene mas de 2 horas
-            let elapsed = new Elapsed(thread.data.date, new Date());
-            if(elapsed.hours.num < 2)
-            {
-                // El hilo tiene menos de 2 horas de antigüedad, mejor vigilarlo un poco...
-                new ArchiverTimer(thread.data);
-            }
-            // Reportar al navegador
-            if (that.current.by.connected) {
-                that.current.by.emit('archiverDone');
-            }
-            // Avanzar en la cola
-            that.current = {};
-            if (that.queue.length > 0) that.next();
-        }));
+            this.reportProgress('Guardando archivos...', ++thread.saved, thread.data.replyCount + 1);
+        }
+
+        // Almacenar imagen de OP
+        await this.storeAttachment(thread.data);
+
+        // Almacenar en la base de datos
+        const query = { 'postId': thread.data.postId, 'board': thread.data.board };
+        thread.data.lastUpdate = Date.now();
+        await Thread.findOneAndUpdate(query, thread.data, { upsert: true });
+
+        // Reportar al navegador
+        if (this.current.by.connected) {
+            this.current.by.emit('archiverDone');
+        }
+        // Avanzar en la cola
+        this.current = {};
+        if (this.queue.length > 0) this.next();
     }
 
     // Reportar progreso al navegador
@@ -133,64 +74,43 @@ class Archiver {
     }
 
     // Guardar archivos adjuntos de un post
-    storeAttachment(post, callback) {
-        let that = this;
-        if (!post.file) return callback();
-        async.waterfall([
-            // Crear Directorios para archivos, si no existen
-            cb => mkdirp(that.current.fileDir, cb),
-            (made, cb) => mkdirp(that.current.thumbDir, cb),
-            // Almacenar thumb
-            (made, cb) => {
-                // Ubicación final de la thumb
-                let fileName  = post.file.thumb.split('/').reverse()[0];
-                let thumbPath = that.current.thumbDir + fileName;
-                that.current.thumbs.push(fileName);
-                fs.exists(thumbPath, exists => {
-                    if (exists) { 
-                        cb(); 
-                    }
-                    else
-                    {
-                        // Descargar thumb
-                        request(post.file.thumb).on('response', ()=>{cb()}).on('error', ()=>{cb()}).pipe(fs.createWriteStream(thumbPath));
-                    }
-                    // Establecer nueva ubicación
-                    post.file.thumb = thumbPath;
-                });
-            },
-            // Almacenar archivo
-            cb => {
-                // Ubicación final del archivo
-                let fileName  = post.file.url.split('/').reverse()[0];
-                let filePath = that.current.fileDir + post.file.url.split('/').reverse()[0];
-                that.current.files.push(fileName);
-                fs.exists(filePath, exists => {
-                    if (exists) {
-                        cb();
-                    }
-                    else{
-                        // Descargar archivo
-                        request(post.file.url).on('end',  ()=>{cb()}).on('error', ()=>{cb()}).pipe(fs.createWriteStream(filePath));
-                    }
-                    // Establecer nueva ubicación
-                    post.file.url = filePath;
-                });
-            },
-            cb => {
-                const filePath = that.current.fileDir + post.file.url.split('/').reverse()[0];
-                fs.createReadStream(filePath).
-                  pipe(crypto.createHash('md5').setEncoding('hex')).
-                  on('finish', function () {
-                    post.file.md5 = this.read();
-                    cb();
-                  });                
-            }
-        ], err => {
-            that.reportProgress('Guardando archivos...', ++that.current.saved, that.current.data.replyCount + 1);
-            callback(null, post);
-        });
+    async storeAttachment(post) {
+        // Crear Directorios para archivos, si no existen
+        mkdirp.sync(this.current.fileDir);
+        mkdirp.sync(this.current.thumbDir);
+
+        // Almacenar thumb
+        // Ubicación final de la thumb
+        const thumbPath = this.current.thumbDir + post.file.thumb.split('/').reverse()[0];
+
+        if (!fs.existsSync(thumbPath)) {
+            // Descargar thumb
+            await downloadFile(post.file.thumb, thumbPath);
+        }
+        // Establecer nueva ubicación
+        post.file.thumb = thumbPath;
+
+        // Almacenar archivo
+        // Ubicación final del archivo
+        const filePath = this.current.fileDir + post.file.url.split('/').reverse()[0];
+
+        if (!fs.existsSync(filePath)) {
+            // Descargar archivo
+            await downloadFile(post.file.url, filePath);
+            post.file.md5 = md5(fs.readFileSync(filePath));
+        }
+        // Establecer nueva ubicación
+        post.file.url = filePath;
     }
 }
+
+const downloadFile = (url, path) => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .pipe(fs.createWriteStream(path))
+            .on('finish', () => { resolve() })
+            .on('error', error => { reject(error) });
+    });
+};
 
 module.exports = new Archiver();
